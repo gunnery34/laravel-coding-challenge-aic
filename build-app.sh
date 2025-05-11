c8d78565aa043b63f73c8b56f4f8b6231ca97d5f
GunnerY <arigho34@gmail.com>
2025-05-11 16:36:32 +0700

    chore: add shell scripts for worker, cron, and build tasks
    - Add `run-worker.sh`, `run-cron.sh`, and `build-app.sh` scripts to automate common Laravel tasks.
    - These scripts handle queue processing, scheduling, and application building respectively, improving deployment and maintenance workflows.

---
+++ build-app.sh
@@ -0,0 +1,14 @@
+# Exit the script if any command fails
+set -e
+
+# Build assets using NPM
+npm run build
+
+# Clear cache
+php artisan optimize:clear
+
+# Cache the various components of the Laravel application
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
